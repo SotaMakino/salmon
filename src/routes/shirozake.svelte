648bceaa29d0d088b36047065116cd5514ab600e
Sota Makino <sotamakino@hey.com>
2021-05-06 20:12:05 +0900

    add canvas

--- src/routes/shirozake.svelte
+++ src/routes/shirozake.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Header from '../components/Header.svelte';
 	import Slider from '../components/Slider.svelte';
+
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
+
+	const imgWidth = 600;
+	const imgHeight = 500;
+
+	const draw = () => {
+		ctx = canvas.getContext('2d');
+		const img = new Image();
+		img.src = `sirozake/1.jpg`;
+		const drawImageActualSize = () => {
+			canvas.width = imgWidth;
+			canvas.height = imgHeight;
+			ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+		};
+		img.onload = drawImageActualSize;
+	};
+
+	onMount(() => {
+		draw();
+	});
 </script>
 
 <svelte:head>
@@ -8,4 +31,5 @@
 </svelte:head>
 
 <Header image="interface/title_siro.png" />
+<canvas bind:this={canvas} />
 <Slider />
